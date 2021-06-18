d56bc99b588c9a6d79789f4ba819655b703006b5
Matt Leung <8941112+mdleung@users.noreply.github.com>
2021-06-18 13:37:56 -0700

    Notes on Data
    -Only YouTube ID needed

--- popup.js
+++ popup.js
@@ -3,9 +3,23 @@
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    window.addEventListener("load", onload, false);
+    window.addEventListener("load", onclick, false);
 
-    function setCount(res) {
+    function doThing(res) {
+      
+      
+      
+
+      var s = document.createElement('script');
+      s.setAttribute('src','jquery-3.6.0.min.js');
+      document.body.appendChild(s);
+
+      var g = document.createElement('script');
+      g.setAttribute('src','handlebars-v4.7.7.js');
+      document.body.appendChild(g);
+
+
+      //Retrieve the template data from the HTML .
 
 
 
@@ -40,20 +54,33 @@ document.addEventListener(
       document.body.append(linebreak);
 
 
-      document.body.append(res.verse.quote);
+      document.body.append(verse);
       document.body.append(linebreak);
 
+      // const video = document.createElement("<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='${sermon_url}' frameborder='0' allowFullScreen></iframe>")
+
+
+      // document.body.append(video);
+      
 
 
 
-      document.body.append(res.video.sermon_url);
-
+      var obj = {"video": {
+        "value": `<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src=https://www.youtube.com/embed/${sermon_url} frameborder='0' allowFullScreen></iframe>`
+      }}
+      document.body.append(obj.video.value);
 
     }
 
-    function onload() {
+    function onclick() {
+
+      
+      console.log(chrome.tabs)
+
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, "", setCount);
+
+        
+        chrome.tabs.sendMessage(tabs[0].id, "", doThing);
 
 
       });
